d0b0b55cb19b114b8205b46c9adc060cb0b5b5fa
Rasseog <125932020+Rasseog@users.noreply.github.com>
2026-01-26 09:49:21 +0200

    lisään assignment 1-2

---
+++ Ohjelmisto_1/Module 2/assignment-4.py
@@ -0,0 +1,10 @@
+import math
+num1 = float(input("Enter the first integer: "))
+num2 = float(input("Enter the second integer: "))
+num3 = float(input("Enter the third integer: "))
+sum_of_numbers = num1 + num2 + num3
+product_of_numbers = num1 * num2 * num3
+average_of_numbers = sum_of_numbers / 3
+print("The sum of the numbers:",int(sum_of_numbers))
+print("The product of the numbers:",int(product_of_numbers))
+print("The average of the numbers:",(average_of_numbers))
